421796ddbac6323d93b8a0f4610f03e11f22ddb7
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-27 12:45:17 -0500

    Reemplazar vista de texto en HomeScreen con fondo e imagen animada

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {ImageBackground, StyleSheet} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {commonStyles} from '../styles/commonStyles';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {screenWidth} from '../utils/Constants';
+import {useIsFocused} from '@react-navigation/native';
 
-const HomeScreen = () => {
+const HomeScreen: FC = () => {
+  const isFocused = useIsFocused();
+  const translateY = useSharedValue(-200);
+
+  useEffect(() => {
+    translateY.value = withTiming(0, {duration: 3000});
+  }, [isFocused]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateY: translateY.value}],
+  }));
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      source={require('../assets/images/b2.png')}
+      style={commonStyles.simpleContainer}>
+      <Animated.Image
+        source={require('../assets/images/banner.png')}
+        style={[styles.img, animatedStyle]}
+      />
+    </ImageBackground>
+  );
+};
 
-export default HomeScreen
+const styles = StyleSheet.create({
+  img: {
+    width: screenWidth,
+    height: screenWidth * 0.8,
+    position: 'absolute',
+    resizeMode: 'contain',
+    top: -20,
+  },
+});
+
+export default HomeScreen;
