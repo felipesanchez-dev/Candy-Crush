2feaae78a0391c3b3946ab3021063ce97a4d3500
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-27 12:21:42 -0500

    Agregar componente de navegación y eliminar vista de texto en App

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {navigationRef} from '../utils/NavigationUtil';
+import SplashScreen from '../screens/SplashScreen';
+import HomeScreen from '../screens/HomeScreen';
+import LevelScreen from '../screens/LevelScreen';
+import GameScreen from '../screens/GameScreen';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="SplashScreen">
+            
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+
+        <Stack.Screen
+          name="HomeScreen"
+          options={{
+            animation: 'fade',
+          }}
+          component={HomeScreen}
+        />
+
+        <Stack.Screen
+          name="LevelScreen"
+          options={{
+            animation: 'fade',
+          }}
+          component={LevelScreen}
+        />
+
+        <Stack.Screen
+          name="GameScreen"
+          options={{
+            animation: 'fade',
+          }}
+          component={GameScreen}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
