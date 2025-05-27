4491b6cab24041c917ba8f2ba0a5831a9629927b
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-27 12:29:18 -0500

    Reemplazar vista de texto en SplashScreen con fondo e imagen de logo

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { FC, useEffect } from 'react'
+import { Image, ImageBackground } from 'react-native'
+import { commonStyles } from '../styles/commonStyles'
+import { resetAndNavigate } from '../utils/NavigationUtil'
 
-const SplashScreen = () => {
+const SplashScreen:FC = () => {
+
+  useEffect(() => {
+    const timoutId = setTimeout(() => {
+      resetAndNavigate('HomeScreen')
+    }, 2500)
+
+    return () => clearTimeout(timoutId)
+  }, [])
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
+    <ImageBackground source={require('../assets/images/bg.png')} style={commonStyles.container}>
+      <Image  source={require('../assets/text/logo.png')} style={commonStyles.img}/>
+    </ImageBackground>
   )
 }
 
