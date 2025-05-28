f38b08c803f9e58ddc0f305d6c69882df995a688
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-28 09:21:46 -0500

    Agregar funcionalidad al encabezado del juego para mostrar la cantidad de caramelos recolectados y el tiempo restante; refactorizar estilos y estructura.

--- src/components/game/GameHeader.tsx
+++ src/components/game/GameHeader.tsx
@@ -1,8 +1,9 @@
 import {View, StyleSheet, Image, ImageBackground} from 'react-native';
 import React, {FC} from 'react';
-import {screenHeight} from '../../utils/Constants';
+import {FONTS, formatTime, screenHeight} from '../../utils/Constants';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {RFValue} from 'react-native-responsive-fontsize';
+import {Text} from 'react-native-gesture-handler';
 
 const GameHeader: FC<{
   totalCount: number;
@@ -18,9 +19,20 @@ const GameHeader: FC<{
       />
       <ImageBackground
         source={require('../../assets/images/lines.jpg')}
-        style={styles.lines}
-      >
+        style={styles.lines}>
+        <View style={styles.subContainer}>
+          
+          <View style={styles.textContainer}>
+            <Text style={styles.candiesText}>
+              🍬 {collectedCandies} /
+              <Text style={styles.totalCandiesText}> {totalCount} </Text>
+            </Text>
+          </View>
 
+          <View style={styles.timeContainer}>
+            <Text style={styles.timerText}>⏳ {formatTime(time)}</Text>
+          </View>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -30,7 +42,6 @@ const styles = StyleSheet.create({
   container: {
     height: screenHeight * 0.15,
     width: '100%',
-    backgroundColor: 'red',
   },
   img: {
     width: RFValue(60),
@@ -42,13 +53,58 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   lines: {
-    padding: 5, 
+    padding: 5,
     borderRadius: 10,
     resizeMode: 'contain',
     overflow: 'hidden',
     margin: RFValue(10),
     marginTop: RFValue(20),
-  }
+  },
+  subContainer: {
+    backgroundColor: '#EDC1B9',
+    padding: RFValue(5),
+    borderRadius: RFValue(12),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#c2978f',
+    padding: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  candiesText: {
+    fontSize: RFValue(14),
+    fontFamily: FONTS.Lily,
+    color: '#3A0E4C',
+  },
+  totalCandiesText: {
+    fontSize: RFValue(12),
+    fontFamily: FONTS.Lily,
+    color: '#3A0E4C',
+  },
+  timeContainer: {
+    alignItems: 'center',
+    backgroundColor: '#c2978f',
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  timerText: {
+    fontSize: RFValue(13),
+    fontFamily: FONTS.Lily,
+    color: '#5B2333',
+  },
 });
 
 export default GameHeader;
