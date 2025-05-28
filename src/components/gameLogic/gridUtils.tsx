3e87e033f4ce443a9ead804a6debeded5a5b7a93
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-28 11:07:40 -0500

    Agregar componente gridUtils para la lógica del juego.

---
+++ src/components/gameLogic/gridUtils.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const gridUtils = () => {
+  return (
+    <View>
+      <Text>gridUtils</Text>
+    </View>
+  )
+}
+
+export default gridUtils
+
+const styles = StyleSheet.create({})
