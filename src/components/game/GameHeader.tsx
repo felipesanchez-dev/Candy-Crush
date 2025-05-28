eb8cf7f9d7c6bd903acb6bddaeb6d7a9d0858273
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-27 20:04:13 -0500

    Agregar pantalla de juego con encabezado y estado inicial; implementar fondo de pantalla

---
+++ src/components/game/GameHeader.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GameHeader = () => {
+  return (
+    <View>
+      <Text>GameHeader</Text>
+    </View>
+  )
+}
+
+export default GameHeader
--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {StyleSheet, ImageBackground} from 'react-native';
+import React, {useState} from 'react';
+import {commonStyles} from '../styles/commonStyles';
+import GameHeader from '../components/game/GameHeader';
 
 const GameScreen = () => {
-  return (
-    <View>
-      <Text>GameScreen</Text>
-    </View>
-  )
-}
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const [time, setTimer] = useState<any>(null);
+  const [collectedCandies, setCollectedCandies] = useState<number>(0);
 
-export default GameScreen
+  return (
+    <ImageBackground
+      style={commonStyles.simpleContainer}
+      source={require('../assets/images/b1.png')}>
+      <GameHeader
+        totalCount={totalCount}
+        collectedCandies={collectedCandies}
+        time={time}
+      />
+    </ImageBackground>
+  );
+};
+const styles = StyleSheet.create({});
+export default GameScreen;
