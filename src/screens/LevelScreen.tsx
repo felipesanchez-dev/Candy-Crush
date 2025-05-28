f480a89d2479d4c0643c0286dd074ff99095c5f9
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-27 19:55:21 -0500

    Implementar pantalla de niveles con navegación y lista de niveles; mejorar estilos y estructura de datos

--- src/screens/LevelScreen.tsx
+++ src/screens/LevelScreen.tsx
@@ -1,12 +1,103 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {FC} from 'react';
+import {FlatList, Image, ImageBackground, Text, View} from 'react-native';
+import {commonStyles} from '../styles/commonStyles';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {levelStyles} from '../styles/levelStyles';
+import ScalePress from '../components/ui/ScalePress';
+import {goBack, navigate} from '../utils/NavigationUtil';
+import {useLevelStore} from '../state/useLevelStore';
+import { gameLevels } from '../utils/data';
 
-const LevelScreen = () => {
+const LevelScreen: FC = () => {
+
+  const {levels} = useLevelStore();
+
+  const levelPressHandler = (id: string) => {
+    const levelKey = `level-${id}` as keyof GameLevels;
+    const level = gameLevels[levelKey];
+    navigate('GameScreen', {
+      level: {...level, id: id}
+    })
+  }
+
+  const renderItem = ({item}: any) => {
+    
+    const opacity = item?.unlocked ? 1 : 0.5;
+    const emoji = item?.completed ? '✅' : item?.unlocked ? '🍬' : '🔒';
+
+    return (
+      <ScalePress
+        style={levelStyles.levelItem}
+        onPress={() => {
+          if (item?.unlocked) {
+            levelPressHandler(item?.id)
+          }
+        }}>
+        <View style={{opacity}}>
+          <Text style={levelStyles.levelText}>{emoji}</Text>
+
+          <Text style={levelStyles.levelText}>
+            Nivel {item?.id}
+          </Text>
+
+          {item?.heighScore > 0 && (
+            <Text style={levelStyles.levelText}>
+              HS: {item?.heighScore}
+            </Text>
+          )}
+
+        </View>
+      </ScalePress>
+    );
+  };
+
   return (
-    <View>
-      <Text>LevelScreen</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      style={commonStyles.container}
+      source={require('../assets/images/forest.jpeg')}>
+      <SafeAreaView />
+      <View style={levelStyles.flex1}>
+        <ScalePress onPress={() => goBack()}>
+          <Image
+            source={require('../assets/icons/back.png')}
+            style={levelStyles.backIcon}
+          />
+        </ScalePress>
+        <ImageBackground
+          source={require('../assets/images/lines.jpg')}
+          style={levelStyles.levelContainer}>
+          <View style={levelStyles.subLevelContainer}>
+            <FlatList
+              data={levels}
+              renderItem={renderItem}
+              showsVerticalScrollIndicator={false}
+              keyExtractor={item => item.id.toString()}
+              numColumns={2}
+              contentContainerStyle={levelStyles.columnWrapper}
+              ListFooterComponent={
+                <View style={levelStyles.comingSoonContainer}>
+                  <Image
+                    source={require('../assets/images/doddle.png')}
+                    style={levelStyles.doddle}
+                  />
+                  <Text style={levelStyles.levelText}>
+                    ¡Próximamente! Mas niveles
+                  </Text>
+                </View>
+              }
+            />
+          </View>
+        </ImageBackground>
 
-export default LevelScreen
+        <View style={levelStyles.flex2}>
+          <Text style={levelStyles.text}>
+            ¡Recoge la cantidad mínima de dulces antes de que el tiempo se
+            acabe!
+          </Text>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default LevelScreen;
