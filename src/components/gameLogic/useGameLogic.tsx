157392a0147b522d377c08bf10af2e906d9e5c0c
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-28 11:08:33 -0500

    Agregar el componente useGameLogic para gestionar la lógica del juego.

---
+++ src/components/gameLogic/useGameLogic.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const useGameLogic = () => {
+  return (
+    <View>
+      <Text>useGameLogic</Text>
+    </View>
+  )
+}
+
+export default useGameLogic
+
+const styles = StyleSheet.create({})
