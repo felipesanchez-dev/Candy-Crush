fbc72146694a3cccdf2c24bfc14c6b494bd11ad9
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-27 20:31:04 -0500

    Refactor GameHeader component to use TypeScript; add props for totalCount, collectedCandies, and time

--- src/components/game/GameHeader.tsx
+++ src/components/game/GameHeader.tsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, StyleSheet, Image, ImageBackground} from 'react-native';
+import React, {FC} from 'react';
+import {screenHeight} from '../../utils/Constants';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {RFValue} from 'react-native-responsive-fontsize';
 
-const GameHeader = () => {
+const GameHeader: FC<{
+  totalCount: number;
+  collectedCandies: number;
+  time: number;
+}> = ({totalCount, collectedCandies, time}) => {
   return (
-    <View>
-      <Text>GameHeader</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <SafeAreaView />
+      <Image
+        source={require('../../assets/icons/hangrope.png')}
+        style={styles.img}
+      />
+      <ImageBackground
+        source={require('../../assets/images/lines.jpg')}
+        style={styles.lines}
+      >
 
-export default GameHeader
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: screenHeight * 0.15,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  img: {
+    width: RFValue(60),
+    height: RFValue(80),
+    resizeMode: 'contain',
+    position: 'absolute',
+    zIndex: 2,
+    top: -RFValue(0),
+    alignSelf: 'center',
+  },
+  lines: {
+    padding: 5, 
+    borderRadius: 10,
+    resizeMode: 'contain',
+    overflow: 'hidden',
+    margin: RFValue(10),
+    marginTop: RFValue(20),
+  }
+});
+
+export default GameHeader;
