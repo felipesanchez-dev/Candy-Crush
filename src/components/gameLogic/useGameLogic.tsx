937b8b8dce786529d139369f94b12fc775dcf505
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-29 17:21:31 -0500

    Agregar lógica de animación y manejo de gestos en el componente useGameLogic.

--- src/components/gameLogic/useGameLogic.tsx
+++ src/components/gameLogic/useGameLogic.tsx
@@ -1,13 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {useRef} from 'react';
+import {Animated} from 'react-native';
 
-const useGameLogic = () => {
-  return (
-    <View>
-      <Text>useGameLogic</Text>
-    </View>
-  )
-}
-const styles = StyleSheet.create({})
-export default useGameLogic
+const useGameLogic = (data: any[][], setData: (data: any) => any) => {
+  const animatedValues = useRef(
+    data?.map(row =>
+      row.map(tile =>
+        tile === null
+          ? null
+          : {x: new Animated.Value(0), y: new Animated.Value(0)},
+      ),
+    ),
+  );
 
+  const handleGesture = async (
+    event: any,
+    rowIndex: number,
+    colIndex: number,
+    state: any,
+    setCollectedCandes: any,
+  ) => {};
+
+  return {
+    handleGesture,
+    animatedValues,
+  };
+};
+
+export default useGameLogic;
