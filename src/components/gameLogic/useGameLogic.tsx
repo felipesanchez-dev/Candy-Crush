6494640db735960ba374ef59529b878a77102cdf
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-05-30 11:07:39 -0500

    Agregar animaciones y lógica de intercambio en la función handleSwipe en useGameLogic

--- src/components/gameLogic/useGameLogic.tsx
+++ src/components/gameLogic/useGameLogic.tsx
@@ -2,6 +2,7 @@ import {useRef} from 'react';
 import {Animated} from 'react-native';
 import {State} from 'react-native-gesture-handler';
 import {playSound} from '../../utils/SoundUtility';
+import {RFPercentage} from 'react-native-responsive-fontsize';
 
 const useGameLogic = (data: any[][], setData: (data: any) => any) => {
   const animatedValues = useRef(
@@ -40,9 +41,96 @@ const useGameLogic = (data: any[][], setData: (data: any) => any) => {
       data[rowIndex][colIndex] !== null &&
       data[targetRow][targetCol] !== null
     ) {
-      
-    }
+      const targetTileAnimationX = Animated.timing(
+        animatedValues[targetRow][targetCol]!.x,
+        {
+          toValue: (colIndex - targetCol) * RFPercentage(5),
+          duration: 200,
+          useNativeDriver: true,
+        },
+      );
 
+      const targetTileAnimationY = Animated.timing(
+        animatedValues[targetRow][targetCol]!.y,
+        {
+          toValue: (rowIndex - targetRow) * RFPercentage(5),
+          duration: 200,
+          useNativeDriver: true,
+        },
+      );
+
+      const sourceTileAnimationX = Animated.timing(
+        animatedValues[rowIndex][colIndex]!.x,
+        {
+          toValue: (targetCol - colIndex) * RFPercentage(5),
+          duration: 200,
+          useNativeDriver: true,
+        },
+      );
+
+      const sourceTileAnimationY = Animated.timing(
+        animatedValues[rowIndex][colIndex]!.y,
+        {
+          toValue: (targetRow - rowIndex) * RFPercentage(5),
+          duration: 200,
+          useNativeDriver: true,
+        },
+      );
+      Animated.parallel([
+        sourceTileAnimationX,
+        sourceTileAnimationY,
+        targetTileAnimationX,
+        targetTileAnimationY,
+      ]).start(async () => {
+        [newGrid[rowIndex][colIndex], newGrid[targetRow][targetCol]] = [
+          newGrid[targetRow][targetCol],
+          newGrid[rowIndex][colIndex],
+        ];
+        let matches = await checkForMatches(newGrid);
+        if (matches?.length > 0) {
+          let totalClearedCandies = 0;
+          while (matches?.length > 0) {
+            playSound('candy_clear');
+            totalClearedCandies += matches.length;
+            newGrid = await clearMatches(newGrid, matches);
+            newGrid = await shiftDown(newGrid);
+            newGrid = await fillRandomCandies(newGrid);
+
+            matches = await checkForMatches(newGrid);
+          }
+
+          animatedValues[rowIndex][colIndex]!.x.setValue(0);
+          animatedValues[rowIndex][colIndex]!.y.setValue(0);
+          animatedValues[targetRow][targetCol]!.x.setValue(0);
+          animatedValues[targetRow][targetCol]!.y.setValue(0);
+
+          setData(newGrid);
+          const hasMoves = await hasPossibleMoves(newGrid);
+          if (!hasMoves) {
+            const d = await handleShuffleAndClear(newGrid);
+            newGrid = d.grid;
+            totalClearedCandies += d.clearedMatching;
+            while (!(await hasPossibleMoves(newGrid))) {
+              const p = await handleShuffleAndClear(newGrid);
+              newGrid = p.grid;
+              totalClearedCandies += p.clearedMatching;
+            }
+            setData(newGrid);
+          }
+          setCollectedCandes(
+            (prevCount: number) => prevCount + totalClearedCandies,
+          );
+        } else {
+          animatedValues[rowIndex][colIndex]!.x.setValue(0);
+          animatedValues[rowIndex][colIndex]!.y.setValue(0);
+          animatedValues[targetRow][targetCol]!.x.setValue(0);
+          animatedValues[targetRow][targetCol]!.y.setValue(0);
+
+          setData(data)
+
+        }
+      });
+    }
   };
 
   const handleGesture = async (
