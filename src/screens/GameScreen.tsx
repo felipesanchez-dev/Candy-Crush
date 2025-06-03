fc0a6a84d6411c6f6821f4888baa292dd2ff0828
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-03 12:28:48 -0500

    Agregar lógica de finalización de juego y animaciones de celebración en GameScreen

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,5 +1,5 @@
-import {StyleSheet, ImageBackground} from 'react-native';
-import React, {FC, useEffect, useState} from 'react';
+import {StyleSheet, ImageBackground, Animated, Alert} from 'react-native';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import {commonStyles} from '../styles/commonStyles';
 import GameHeader from '../components/game/GameHeader';
 import {useRoute} from '@react-navigation/native';
@@ -7,6 +7,10 @@ import {useSound} from '../navigation/SoundContext';
 import {gameLevels} from '../utils/data';
 import GameFooter from '../components/game/GameFooter';
 import GameTile from '../components/game/GameTile';
+import {useLevelStore} from '../state/useLevelStore';
+import {goBack} from '../utils/NavigationUtil';
+import { screenWidth } from '../utils/Constants';
+import LottieView from 'lottie-react-native';
 
 const GameScreen: FC = () => {
   const route = useRoute();
@@ -16,6 +20,13 @@ const GameScreen: FC = () => {
   const [totalCount, setTotalCount] = useState<number>(0);
   const [time, setTimer] = useState<any>(null);
   const [collectedCandies, setCollectedCandies] = useState<number>(0);
+  const [showAnimation, setShowAnimation] = useState<boolean>(false);
+  const [firstAnimation, setFirstAnimation] = useState<boolean>(false);
+
+  const {completeLevel, unlockLevel} = useLevelStore();
+
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+  const scaleAnim = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
     if (item?.id) {
@@ -28,6 +39,104 @@ const GameScreen: FC = () => {
       }
     }
   }, [item]);
+
+  useEffect(() => {
+    if (time === 0) {
+      handleGameOver();
+    }
+  }, [time]);
+
+  const handleGameOver = () => {
+    if (collectedCandies >= totalCount) {
+      completeLevel(item?.level?.id, collectedCandies);
+      unlockLevel(item?.level?.id + 1);
+      Alert.alert('🎉 ¡Felicidades!', 'Has completado el nivel con éxito.', [
+        {
+          text: 'Aceptar',
+          onPress: () => goBack(),
+        },
+      ]);
+    } else {
+      Alert.alert(
+        '😢 ¡Juego Terminado!',
+        'No has alcanzado la cantidad de dulces necesarios.',
+        [
+          {
+            text: 'Reintentar',
+            onPress: () => {
+              setCollectedCandies(0);
+              setGridData(gameLevels[`level${item.id}`].grid);
+              setTimer(gameLevels[`level${item.id}`].time);
+            },
+          },
+          {
+            text: 'Salir',
+            onPress: () => goBack(),
+          },
+        ],
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (time && time > 0) {
+      const timerInteval = setInterval(() => {
+        setTimer((prev: number) => {
+          if (prev === 1000) {
+            clearInterval(timerInteval);
+            return 0;
+          }
+          return prev - 1000;
+        });
+      }, 1000);
+      return () => clearInterval(timerInteval);
+    }
+  }, [time]);
+
+  useEffect(() => {
+    if (collectedCandies >= totalCount && totalCount > 0 && !firstAnimation) {
+      setShowAnimation(true);
+      startHeartBeatAnimation();
+    }
+  }, [collectedCandies, totalCount]);
+
+  const startHeartBeatAnimation = () => {
+    playSound('cheer', false);
+    Animated.loop(
+      Animated.sequence([
+        Animated.parallel([
+          Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 800,
+            useNativeDriver: true,
+          }),
+          Animated.timing(fadeAnim, {
+            toValue: 1.2,
+            duration: 800,
+            useNativeDriver: true,
+          }),
+        ]),
+        Animated.parallel([
+          Animated.timing(fadeAnim, {
+            toValue: 0.8,
+            duration: 800,
+            useNativeDriver: true,
+          }),
+          Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 800,
+            useNativeDriver: true,
+          }),
+        ]),
+      ]),
+      {
+        iterations: 2,
+      },
+    ).start(() => {
+      setFirstAnimation(true);
+      setShowAnimation(false);
+    });
+  };
 
   return (
     <ImageBackground
@@ -45,9 +154,48 @@ const GameScreen: FC = () => {
           setCollectedCandies={setCollectedCandies}
         />
       )}
+      {
+        showAnimation && (
+          <>
+            <Animated.Image 
+            source={require('../assets/text/t2.png')}
+            style={[
+              styles.centerImage,
+              {
+                opacity: fadeAnim,
+                transform: [{scale: scaleAnim}]
+              }
+            ]}
+            />
+            <LottieView 
+            source={require('../assets/animations/confetti.json')}
+            style={styles.lottie}
+            autoPlay
+            loop
+            />
+          </>
+        )
+      }
       <GameFooter />
     </ImageBackground>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centerImage: {
+    position: 'absolute',
+    width: screenWidth * 0.8,
+    height: 180,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    top: '15%',
+  },
+  lottie: {
+    position: 'absolute',
+    width: screenWidth * 0.8,
+    height: 180,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    top: '10%'
+  }
+});
 export default GameScreen;
